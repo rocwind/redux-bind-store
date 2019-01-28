a5c90d5604bd212575b854fc680550a544aa2083
Roc Wu <rocwind@gmail.com>
2019-01-28 20:55:15 +0800

    add real test cases

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,54 @@
 import { bindStore } from '../index';
+import { ActionCreator, Action, createStore, Reducer, Store } from 'redux';
 
-it('dummy test', () => {
+enum ActionTypeEnum {
+    Add = 'add',
+    Sub = 'sub',
+}
+
+const reducer: Reducer<number, Action<ActionTypeEnum>> = (
+    state: number = 0,
+    action: Action<ActionTypeEnum>
+) => {
+    switch (action.type) {
+        case ActionTypeEnum.Add:
+            return state + 1;
+        case ActionTypeEnum.Sub:
+            return state - 1;
+    }
+
+    return state;
+}
+
+const add: ActionCreator<Action<ActionTypeEnum>> = () => ({ type: ActionTypeEnum.Add });
+const sub: ActionCreator<Action<ActionTypeEnum>> = () => ({ type: ActionTypeEnum.Sub });
+
+
+let store: Store<number, Action<ActionTypeEnum>>;
+beforeEach(() => {
+    store = createStore(reducer);
+});
+
+it('binded dispatch changes state', () => {
+    expect(store.getState()).toBe(0);
+    const { dispatch } = bindStore(store);
+    dispatch(add());
+    expect(store.getState()).toBe(1);
+});
+
+it('binded getState gets latest state', () => {
+    const { getState } = bindStore(store);
+    expect(getState()).toBe(0);
+    store.dispatch(add());
+    expect(store.getState()).toBe(1);
+});
+
+it('binded subscribe gets updates', done => {
+    const { subscribe } = bindStore(store);
+    subscribe(() => {
+        expect(store.getState()).toBe(1);
+        done();
+    });
+    expect(store.getState()).toBe(0);
+    store.dispatch(add());
 });
